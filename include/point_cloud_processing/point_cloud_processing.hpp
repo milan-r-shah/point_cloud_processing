4a487c9a51f422a7c7cd7f9d555f2548e8db287b
Milan Shah <mrshah@wpi.edu>
2019-11-17 11:18:13 -0500

    Created ROS Subscriber to subscribe to input PointCloud
    01. In the .hpp file, declared subscriber and its callback function
        Also, declared member variables for input pointcloud.
    02. In the .cpp file, initialized subscriber and added definition for
        subscriber callback function.

--- include/point_cloud_processing/point_cloud_processing.hpp
+++ include/point_cloud_processing/point_cloud_processing.hpp
@@ -3,6 +3,9 @@
 #define POINT_CLOUD_PROCESSING
 
 #include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#define NO_OF_POINT_CLOUD 10
 
 class PointCloudProcessing
 {
@@ -13,6 +16,20 @@ public:
 
 private:
     ros::NodeHandle nh_;
+
+    // ROS Subscriber for subscribing to a ROS topic having input PointCloud data
+    // ROS Subscriber Declaration
+    ros::Subscriber sub_cloud_;
+
+    // ROS Subscriber Callback function declaration
+    void subCallback(const sensor_msgs::PointCloud2 &msg);
+
+    // input PointCloud having datatype - sensor_msgs::PointCloud2
+    sensor_msgs::PointCloud2 input_pc2_;
+    std::vector<sensor_msgs::PointCloud2> input_pc2_vec_;
+
+    // flag for checking whether required number of PointCloud has been received
+    bool has_point_cloud;
 };
 
 #endif
--- src/point_cloud_processing.cpp
+++ src/point_cloud_processing.cpp
@@ -4,6 +4,13 @@ PointCloudProcessing::PointCloudProcessing(ros::NodeHandle &nh, ros::NodeHandle 
 {
     nh_ = nh;
     std::cout << "from Constructor \n";
+
+    // ROS Subscriber Initialization
+    // Input ROS topic: /zed/zed_node/point_cloud/cloud_registered
+    sub_cloud_ = nh_.subscribe("/zed/zed_node/point_cloud/cloud_registered", 1, &PointCloudProcessing::subCallback, this);
+
+    // In the beginning, there is no PointCloud, so flag value is 'false'
+    has_point_cloud = false;
 }
 
 PointCloudProcessing::~PointCloudProcessing()
@@ -14,4 +21,19 @@ PointCloudProcessing::~PointCloudProcessing()
 void PointCloudProcessing::runOnce()
 {
     std::cout << "from runOnce() \n";
+}
+
+// ROS Subsriber callback function definition
+void PointCloudProcessing::subCallback(const sensor_msgs::PointCloud2 &msg)
+{
+    // storing input PointCloud into PointCloud vector
+    if (input_pc2_vec_.size() < NO_OF_POINT_CLOUD)
+    {
+        input_pc2_vec_.push_back(msg);
+    }
+    else
+    {
+        // once the required number of PointCloudss has been received set this flag to'true'
+        has_point_cloud = true;
+    }
 }
